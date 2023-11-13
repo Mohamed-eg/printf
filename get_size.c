587dd3b409998c28f0480cd7e21831b8873b2db5
Mohamed-eg <mhmdalshayb39@gmail.com>
2023-11-14 00:12:38 +0200

    mary

---
+++ get_size.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * getSize - Calculates the size to cast the argument
+ * @format: Formatted string in which to print the arguments
+ * @i: List of arguments to be printed.
+ *
+ * Return: Precision.
+ */
+int getSize(const char *format, int *i)
+{
+	int curr_i = *i + 1;
+	int size = 0;
+
+	if (format[curr_i] == 'l')
+		size = longSize;
+	else if (format[curr_i] == 'h')
+		size = shortSize;
+	if (size == 0)
+		*i = curr_i - 1;
+	else
+		*i = curr_i;
+	return (size);
+}
---
+++ get_width.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * getWidth - Calculates the width for printing
+ * @format: Formatted string in which to print the arguments.
+ * @i: List of arguments to be printed.
+ * @list: list of arguments.
+ *
+ * Return: width.
+ */
+int getWidth(const char *format, int *i, va_list list)
+{
+	int curr_i;
+	int width = 0;
+
+	for (curr_i = *i + 1; format[curr_i] != '\0'; curr_i++)
+	{
+		if (IsItDigit(format[curr_i]))
+		{
+			width *= 10;
+			width += format[curr_i] - '0';
+		}
+		else if (format[curr_i] == '*')
+		{
+			curr_i++;
+			width = va_arg(list, int);
+			break;
+		}
+		else
+			break;
+	}
+	*i = curr_i - 1;
+	return (width);
+}

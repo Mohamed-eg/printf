cbb57d178e774567316178b622091800d4cfc9e9
Mohamed-eg <mhmdalshayb39@gmail.com>
2023-11-13 02:51:02 +0200

    rebufer

--- printf.c
+++ printf.c
@@ -1,33 +1,33 @@
 #include "main.h"
-void printBuffer(char buffer[], int *buff_ind);
+void print_buffer(char buffer[], int *buff_ind);
+
 /**
  * _printf - Printf function
  * @format: format.
  * Return: Printed chars.
  */
+
 int _printf(const char *format, ...)
 {
 	int i, printed = 0, printedCharts = 0;
 	int flags, width, precision, size, buff_ind = 0;
 	va_list list;
 	char buffer[buffSize];
-
 	if (format == NULL)
 		return (-1);
 	va_start(list, format);
-
 	for (i = 0; format && format[i] != '\0'; i++)
 	{
 		if (format[i] != '%')
 		{
 			buffer[buff_ind++] = format[i];
 			if (buff_ind == buffSize)
-				printBuffer(buffer, &buff_ind);
+				print_buffer(buffer, &buff_ind);
 			printedCharts++;
 		}
 		else
 		{
-			printBuffer(buffer, &buff_ind);
+			print_buffer(buffer, &buff_ind);
 			flags = getFlags(format, &i);
 			width = getWidth(format, &i, list);
 			precision = getPrecision(format, &i, list);
@@ -40,17 +40,16 @@ int _printf(const char *format, ...)
 			printedCharts += printed;
 		}
 	}
-	printBuffer(buffer, &buff_ind);
+	print_buffer(buffer, &buff_ind);
 	va_end(list);
 	return (printedCharts);
 }
-
 /**
- * printBuffer - Prints the contents of the buffer if it exist
+ * print_buffer - Prints the contents of the buffer if it exist
  * @buffer: Array of chars
  * @buff_ind: Index at which to add next char, represents the length.
  */
-void printBuffer(char buffer[], int *buff_ind)
+void print_buffer(char buffer[], int *buff_ind)
 {
 	if (*buff_ind > 0)
 		write(1, &buffer[0], *buff_ind);

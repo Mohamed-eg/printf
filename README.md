cebf028d18840d62fbee2845148eae5630d802e4
Mohamed-eg <mhmdalshayb39@gmail.com>
2023-11-15 01:04:01 +0200

    last day

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
-readme
+# 🤝Custom `Printf` Function in C
+
+Project Overview
+This project aims to develop a custom `printf` function in C that mimics the functionality of the standard `printf` function but offers additional features and flexibility. This project is a collaborative effort between students at Hulperton School, with significant contributions from Maria and the project's initiator.
+
+## Project Motivation
+
+The standard `printf` function in C is a powerful tool for formatted output, but it can sometimes be limiting in its capabilities. This project seeks to create a custom `printf` function that extends the functionality of the original, providing more options for formatting and output control.
+
+## Key Features
+
+Our custom `printf` function will offer several enhancements over the standard `printf`:
+
+Custom Formatting Flags: We will introduce additional formatting flags to control the appearance of output, such as justification, alignment, and padding.
+
+Enhanced Data Types: We will expand the range of supported data types beyond the standard `printf`, enabling formatted output for user-defined data types and structures.
+
+Variadic Arguments: Our custom `printf` will handle variadic arguments, allowing for flexible formatting of an arbitrary number of arguments.
+
+Error Handling: We will incorporate error handling mechanisms to gracefully deal with invalid format specifications and unexpected input.
+
+## Implementation Approach
+
+The custom `printf` function will be implemented using a similar approach to the standard `printf`, utilizing a parser to interpret the format string and dynamically generate the formatted output. We will extend the parser to handle our custom formatting flags and data types.
+
+## Expected Outcomes
+
+Upon completion of this project, we expect to have a fully functional custom `printf` function that provides enhanced formatting capabilities, supports a wider range of data types, and handles variadic arguments effectively. This custom function will serve as a valuable tool for formatted output in C programming.
+
+## Compilation
+
+```sh
+$ gcc -Wall -Werror -Wextra -pedantic -std=gnu89 *.c
+```
--- printf.c
+++ printf.c
@@ -11,37 +11,38 @@ int _printf(const char *format, ...)
 {
 	int i, printed = 0, printedCharts = 0;
 	int flags, width, precision, size, buff_ind = 0;
-	va_list list;
+	va_list list;/*Declare a va_list object*/
 	char buffer[buffSize];
 
+/**/
 	if (format == NULL)
-		return (-1);
+		return (-1);/*This line checks if the format string is NULL. If it is, it returns -1 to indicate an error.*/
 	va_start(list, format);
 	for (i = 0; format && format[i] != '\0'; i++)
 	{
-		if (format[i] != '%')
+		if (format[i] != '%')/*This line checks if the current character in the format string is not a %.*/
 		{
-			buffer[buff_ind++] = format[i];
+			buffer[buff_ind++] = format[i];/*If the current character is not a %, it is copied to the buffer array at index buff_ind. The buff_ind is then incremented.*/
 			if (buff_ind == buffSize)
-				printBuffer(buffer, &buff_ind);
+				printBuffer(buffer, &buff_ind);/*f the buff_ind reaches the buffSize, it means the buffer is full. In this case, the printBuffer function is called to flush the buffer to the output. The &buff_ind is passed to the function to update the buff_ind after printing.*/
 			printedCharts++;
 		}
 		else
 		{
 			printBuffer(buffer, &buff_ind);
-			flags = getFlags(format, &i);
+			flags = getFlags(format, &i);/*The getFlags function is called to parse the format flags from the format string starting from the current character. The &i is passed to the function to update the i after parsing the flags.*/
 			width = getWidth(format, &i, list);
 			precision = getPrecision(format, &i, list);
-			size = getSize(format, &i);
+			size = getSize(format, &i);/*The getSize function is called to parse the size modifier from the format string starting from the current character. The &i is passed to the function to update the i after parsing the size.*/
 			++i;
 			printed = handle_print(format, &i, list, buffer,
 				flags, width, precision, size);
 			if (printed == -1)
-				return (-1);
+				return (-1);/*If handle_print returns -1, it indicates an error. In this case, the function returns -1 to propagate the error.*/
 			printedCharts += printed;
 		}
 	}
-	printBuffer(buffer, &buff_ind);
+	printBuffer(buffer, &buff_ind);/*The printBuffer function is called to flush any remaining characters in the buffer to the output.*/
 	va_end(list);
 	return (printedCharts);
 }
